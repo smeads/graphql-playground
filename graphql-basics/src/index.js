209beadd91324c20d406d385ea81dc36c264812a
Steve Meads <smeads@nozani.com>
2019-03-14 11:03:42 -0600

    add resolvers and count subscription

--- graphql-basics/src/index.js
+++ graphql-basics/src/index.js
@@ -1,13 +1,30 @@
-import { GraphQLServer } from "graphql-yoga";
+import { GraphQLServer, PubSub } from "graphql-yoga";
+import db from "./db";
 import Query from "./resolvers/query";
+import Mutation from "./resolvers/mutation";
+import Subscription from "./resolvers/subscription";
+import User from "./resolvers/user";
+import Post from "./resolvers/post";
+import Comment from "./resolvers/comment";
+
+const pubsub = new PubSub();
 
 const server = new GraphQLServer({
   typeDefs: "./src/schema.graphql",
   resolvers: {
-    Query
+    Query,
+    Mutation,
+    Subscription,
+    User,
+    Post,
+    Comment
+  },
+  context: {
+    db,
+    pubsub
   }
 });
 
 server.start(() => {
-  console.log("GraphQL server is running...");
+  console.log("The server is up!");
 });
